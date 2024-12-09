c57a6d80f209ae640214a45d798ec86b242ca29d
AntoTiro <104451593+AntoTiro@users.noreply.github.com>
2024-12-09 22:36:29 +0000

    Leaderboard Class implementation

---
+++ core/src/main/java/com/spacecomplexity/longboilife/leaderboard/LeaderboardScreen.java
@@ -0,0 +1,155 @@
+package com.spacecomplexity.longboilife.leaderboard;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.spacecomplexity.longboilife.Main;
+import com.spacecomplexity.longboilife.MainInputManager;
+import com.spacecomplexity.longboilife.game.globals.Window;
+
+/**
+ * Main class to control the menu screen.
+ */
+public class LeaderboardScreen implements Screen {
+    private final Main game;
+
+    private Viewport viewport;
+
+    private Texture backgroundTexture;
+    private SpriteBatch batch;
+
+    private Stage stage;
+    private Skin skin;
+
+    public LeaderboardScreen(Main game) {
+        this.game = game;
+
+        // Initialise viewport and drawing elements
+        viewport = new ScalingViewport(Scaling.fit, Window.DEFAULT_WIDTH, Window.DEFAULT_HEIGHT);
+        stage = new Stage(viewport);
+        batch = new SpriteBatch();
+
+        // Load background texture
+        backgroundTexture = new Texture(Gdx.files.internal("menu/background.png"));
+
+        // Load UI skin for buttons
+        skin = new Skin(Gdx.files.internal("ui/skin/uiskin.json"));
+    }
+
+    @Override
+    public void show() {
+        // Table layout for menu alignment
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        // Initialise play button
+        TextButton playButton = new TextButton("Play", skin, "round");
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Switch to game screen
+                game.switchScreen(Main.ScreenType.GAME);
+            }
+        });
+
+        // Initialise exit button
+        TextButton exitButton = new TextButton("Exit", skin, "round");
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Exit the application
+                Gdx.app.exit();
+            }
+        });
+
+        // Initialise settings button
+        TextButton settingsButton = new TextButton("Settings", skin, "round");
+        settingsButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.openSettings(Main.ScreenType.MENU);
+            }
+        });        
+
+        // Initialise leaderboard button
+        TextButton leaderboardButton = new TextButton("Leaderboard", skin, "round");
+        leaderboardButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.openLeaderboard(Main.ScreenType.MENU);
+            }
+        });
+
+        // Add buttons to table
+        table.add(playButton);
+        table.row();
+        table.add(settingsButton).padTop(10);
+        table.row();
+        table.add(exitButton).padTop(10);
+
+        
+        // Position the table correctly
+        table.pad(150).padBottom((Window.DEFAULT_HEIGHT / 2) - 40).bottom().right();
+
+        // Allows UI to capture touch events
+        InputMultiplexer inputMultiplexer = new InputMultiplexer(new MainInputManager(), stage);
+        Gdx.input.setInputProcessor(inputMultiplexer);
+    }
+
+    @Override
+    public void render(float delta) {
+        // Clear the screen
+        ScreenUtils.clear(0, 0, 0, 1f);
+
+        // Draw background image
+        batch.begin();
+        batch.draw(backgroundTexture, 0, 0, Window.DEFAULT_HEIGHT, Window.DEFAULT_HEIGHT);
+        batch.end();
+
+        // Draw and apply ui
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        Window.update(width, height);
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+        backgroundTexture.dispose();
+        batch.dispose();
+    }
+}
